0676dd42152661f4b858b8084cd0a126086d4016
yehoudah <38001224+yehoudah@users.noreply.github.com>
2020-07-31 17:40:12 +0300

    Create read-more.js

---
+++ read-more.js
@@ -0,0 +1,68 @@
+/*------------ Show / Hide Text ------------*/
+    function showHideText(sSelector, options) {
+        // Def. options
+        var defaults = {
+            charQty: 100,
+            ellipseText: "...",
+            moreText: "Show more",
+            lessText: "Show less"
+        };
+
+        var settings = $.extend({}, defaults, options);
+
+        var s = this;
+
+        s.container = $(sSelector);
+        s.containerH = s.container.height();
+
+        s.container.each(function() {
+            var content = $(this).html();
+
+            if (content.length > settings.charQty) {
+
+                var visibleText = content.substr(0, settings.charQty);
+                var hiddenText = content.substr(settings.charQty, content.length - settings.charQty);
+
+                var html = visibleText +
+                    '<span class="moreellipses">' +
+                    settings.ellipseText +
+                    '</span><span class="morecontent"><span>' +
+                    hiddenText +
+                    '</span><a href="" class="morelink">' +
+                    settings.moreText +
+                    '</a></span>';
+
+                $(this).html(html);
+            }
+
+        });
+       
+        s.showHide = function(event) {
+            event.preventDefault();
+            if ($(this).hasClass("less")) {
+                   $(this).removeClass("less");
+                $(this).html(settings.moreText);
+                $(this).prev().slideUp('fast', function() {
+                    $(this).parent().prev().fadeIn();
+                });
+            } else {
+                $(this).addClass("less");
+                $(this).html(settings.lessText);
+                $(this).parent().prev().hide();
+                $(this).prev().slideDown('fast');                
+            }
+        }
+
+        $(".morelink").bind('click', s.showHide);
+    }
+    /*------------------------------------------*/
+    
+/*------------------usage example------------------------*/
+   new showHideText('.content', {
+
+        charQty: 400,
+        ellipseText: "...",
+        moreText: "Read more...",
+        lessText: "Read less..."
+
+    });
